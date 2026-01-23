9fd1f22ed81658e9707f1d3e20229ba5d88a0967
ZulkaifAhmad <zulkaifkhan183@gmail.com>
2026-01-24 00:50:39 +0500

    add new code

---
+++ src/assets/assets.js
@@ -0,0 +1,25 @@
+import black_suite_girl from "./black_suite_girl.png"
+import building_view from "./building_view.png"
+import classroom from "./classroom.jpg"
+import Girl_books from "./Girl_books.jpg"
+import Girl_headphone_smiling from "./Girl_headphone_smiling.png"
+import Girl_Standing from "./Girl_Standing.png"
+import Landing from "./Landing.jpg"
+import Logo from "./Logo.png"
+import Man from "./Man.png"
+import Man2 from "./Man2.jpg"
+import Man3 from "./Man3.png"
+import Women_smiling from "./Women_smiling.jpg"
+
+export const assets = {
+    black_suite_girl , 
+    building_view , 
+    classroom ,
+    Girl_books , 
+    Girl_Standing , 
+    Girl_headphone_smiling , 
+    Landing , 
+    Logo , 
+    Man , Man2 , Man3 ,
+    Women_smiling
+}
